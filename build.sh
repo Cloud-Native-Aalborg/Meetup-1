86257e4fdd4f2e36bfa64220b623899ccf741b3f
mejlholm <mejlholm@mejlholm.org>
2019-08-31 19:20:44 +0200

    Move to workloads dir for flux to find changes.

--- build.sh
+++ build.sh
@@ -5,15 +5,11 @@ set -e
 cd wisdom-service || exit
 sudo docker build . -t mejlholm/wisdom-service:1.0-SNAPSHOT
 sudo docker push mejlholm/wisdom-service:1.0-SNAPSHOT
-kubectl delete -f deploy
-kubectl apply -f deploy
 cd .. || exit
 
 cd wisdom-frontend || exit
 sudo docker build . -t mejlholm/wisdom-frontend:1.0-SNAPSHOT
 sudo docker push mejlholm/wisdom-frontend:1.0-SNAPSHOT
-kubectl delete -f deploy
-kubectl apply -f deploy
 cd .. || exit
 
 
