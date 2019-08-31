fddf0a27589d4d4b24a69b2177a7edf536d9584c
mejlholm <mejlholm@mejlholm.org>
2019-08-31 22:31:12 +0200

    Fix broken deploymennt.yaml

--- build.sh
+++ build.sh
@@ -7,14 +7,19 @@ version=$(git rev-parse HEAD)
 for application in "wisdom-frontend" "wisdom-service"
 do
   cd ${application} || exit
+
+  #build and push to docker hub
   sudo docker build . -t mejlholm/${application}:"${version}"
   sudo docker push mejlholm/${application}:"${version}"
 
+  #replace version in deployment.yaml
   sed -i -E "s,image: mejlholm/$application:(.*)$,image: mejlholm/$application:$version,g" deploy/${application}-deployment.yaml
+
+  #add, commit and push to github
   git add deploy/${application}-deployment.yaml
   git commit -m "build"
-
   git push origin master
+
   cd .. || exit
 done
 
