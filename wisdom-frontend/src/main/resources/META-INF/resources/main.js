c231d42c4ff25b03ca7599be3856070838899ad1
mejlholm <mejlholm@mejlholm.org>
2019-08-31 22:34:17 +0200

    Fix url

--- wisdom-frontend/src/main/resources/META-INF/resources/main.js
+++ wisdom-frontend/src/main/resources/META-INF/resources/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $.getJSON({url: "http://wisdom.debian-2gb-nbg1-1.mejlholm.org/wisdom/random", success: function(result) {
+    $.getJSON({url: "http://" + window.location.hostname + "/wisdom/random", success: function(result) {
         $("#quote").html(result['message']);
   }});
 });
