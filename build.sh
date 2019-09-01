8e7728092f3b9fbc4f7c987501b02f7932207250
mejlholm <mejlholm@mejlholm.org>
2019-09-01 10:10:43 +0200

    Try script instead.

--- build.sh
+++ build.sh
@@ -8,16 +8,12 @@ for application in "wisdom-frontend" "wisdom-service"
 do
   cd ${application} || exit
 
-  #build our multistage docker images and push to docker hub
-  sudo docker build . -t mejlholm/${application}:"${version}"
-  sudo docker push mejlholm/${application}:"${version}"
-
   #replace version in deployment.yaml
   sed -i -E "s,image: mejlholm/$application:(.*)$,image: mejlholm/$application:$version,g" deploy/${application}-deployment.yaml
 
   #add, commit and push to github
   git add deploy/${application}-deployment.yaml
-  git commit -m "build"
+  git commit -m "Drone bumped image tags automatically"
   git push origin master
 
   cd .. || exit
